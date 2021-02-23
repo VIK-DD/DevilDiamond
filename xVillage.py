fe3213d79abf75d6f35364ff96ffc508be9745dd
VictorLCT952 <57416797+VictorLCT952@users.noreply.github.com>
2021-02-23 11:12:10 -0800

    Update xVillage.py

--- xVillage.py
+++ xVillage.py
@@ -6,7 +6,7 @@ client = commands.Bot(command_prefix = '')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Streaming(name="xVillage", url="https://www.twitch.tv/the8bitdrummer"))
-print('xVillage is Ready')
+    await client.change_presence(activity=discord.Streaming(name="Diamonds", url="https://www.twitch.tv/the8bitdrummer"))
+print('DevilDiamond is Ready')
 
 client.run(os.getenv('TOKEN'))
